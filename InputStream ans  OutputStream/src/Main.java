4bafef242c6528c8ef094501e6067c1aa7557751
Appolo-13 <rayl1x.gta@gmail.com>
2024-11-12 09:21:40 +0100

    byteBuffer updated commit

--- InputStream ans  OutputStream/src/Main.java
+++ InputStream ans  OutputStream/src/Main.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -44,21 +46,27 @@ public class Main {
         readAllContacts();
 
     }
-    public static List<Contact> readAllContacts() {
+    public static List<Contact> readAllContacts() throws IOException {
         List<Contact> contactList = new ArrayList<Contact>();
-        Path filePath = Path.of("contacts.txt");
-
-        try {
-            List<String> string = Files.readAllLines(filePath);
-
-            for (String contact : string) {
-                String[] words = contact.split(",");
-                contactList.add(new Contact(words[0], words[1]));
-                System.out.println("The contacts in the file: "+ contact);
+        try (FileChannel channel = (FileChannel) Files.newByteChannel(Paths.get("contacts.txt"))){
+            MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+            StringBuilder builder = new StringBuilder("");
+            for(int i = 0; i < channel.size(); i++){
+                builder.append((char) buffer.get());
             }
 
+            // System.out.println(builder.toString());
+            String result = builder.toString().replaceAll("[\\[\\]]", "");
+            String result2 = result.replaceAll("]", "");
+            String[] words = result2.toString().split(", "); 
+            for (String string : words) {
+                String[] nameNumber = string.split(",");
+                contactList.add(new Contact(nameNumber[0], nameNumber[1]));
+            }
+            System.out.println(Arrays.toString(words));
+
         } catch (IOException exception) {
-            System.out.println("Error with reading contact list.");
+            System.out.println("Error.");
             exception.printStackTrace();
         }
         return contactList;
